e6849fa9e280fb9afd347536c05dfe550e225b22
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-18 19:40:23 +0100

    Removes method from dinosaur as now in park

--- models/dinosaur.js
+++ models/dinosaur.js
@@ -4,15 +4,4 @@ const Dinosaur = function (species, diet, guestsAttractedPerDay) {
   this.guestsAttractedPerDay = guestsAttractedPerDay;
 }
 
-Dinosaur.prototype.getMostVisited = function(dinosaurs) {
-  let visits = 0;
-  let mostVisited;
-  for(let dino of dinosaurs) {
-    if(dino.guestsAttractedPerDay > visits) {
-      mostVisited = dino;
-    }
-  }
-  return mostVisited;
-}
-
 module.exports = Dinosaur;
